5c4ec00f5713bd9cf17bc85801618013fa8202e1
Nikos Gkikizas <nikos.gkikizas@satalia.com>
2023-06-26 13:21:22 +0300

    changed setup.py and src files

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("requirements.txt") as f:
     requirements = [line.rstrip() for line in f.readlines()]
 
 setup(
-    name = "concrete_image_segmentation",
+    name = "concrete",
     version = "1.0.0",
     author = "Nikos Gkikizas",
     description="Segmentation of 3d concrete sample scans",
     author_email = "nickgikizas@hotmail.com",
     install_requires = requirements,
-    packages = find_packages(where="src"),
     package_dir={"": "src"},
+    packages = find_packages(where='src'),
     python_requires=">=3.10",
     # entry_points = (
         
