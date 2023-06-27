e226b1afa7aae932d9762e152145033a327f9d89
Nikos Gkikizas <nikos.gkikizas@satalia.com>
2023-06-27 19:56:45 +0300

    fix with determinism

--- src/concrete_image_segmentation/data.py
+++ src/concrete_image_segmentation/data.py
@@ -26,8 +26,8 @@ def set_seed(seed=0):
     tf_rand.set_seed(seed)
     set_random_seed(seed)
     random.seed(seed)
-    os.environ["TF_DETERMINISTIC_OPS"] = "1"
-    os.environ["TF_CUDNN_DETERMINISM"] = "1"
+    # os.environ["TF_DETERMINISTIC_OPS"] = "1"
+    # os.environ["TF_CUDNN_DETERMINISM"] = "1"
     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
